987ac1de5199cc37a2e8db4d4962a91681042d8f
Felix Yuan <Felix@FELIXs-MacBook-Air.local>
2012-12-20 21:02:05 -0500

    Added some model code

--- python/writer/artillery/models.py
+++ python/writer/artillery/models.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOne(User, primary_key=True)
-    description = models.CharField(max_length=140)
+    user = models.ForeignKey(User, primary_key=True)
+    description = models.TextField(blank=True)
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_on = models.DateField()
+    modified_on = models.DateField()
+
+
+class Canon(model.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    author = models.ForeignKey(User)
+    is_nsfw = models.BooleanField(default=False)
+    primary_genre = models.ForeignKey(Genre)
