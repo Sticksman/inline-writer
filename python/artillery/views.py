57b9f8673b087440bef486f8ad0e564c88615b83
Felix Yuan <Felix@FELIXs-MacBook-Air.local>
2013-01-20 20:50:41 -0800

    Added a navbar with base.html

--- python/artillery/views.py
+++ python/artillery/views.py
@@ -2,6 +2,8 @@
 from django import shortcuts
 from django import template
 
+from artillery import models
+
 def home(request):
+    stories = models.Story.objects.all()
     return shortcuts.render_to_response('base.html',{})
-    pass
