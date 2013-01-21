9d1118614cacd86da33a17b3d3f3f20f6c68d782
Felix Yuan <Felix@FELIXs-MacBook-Air.local>
2013-01-20 21:07:32 -0800

    Added a write page.

--- python/artillery/views.py
+++ python/artillery/views.py
@@ -4,6 +4,11 @@ from django import template
 
 from artillery import models
 
+
 def home(request):
     stories = models.Story.objects.all()
     return shortcuts.render_to_response('artillery/base.html',{})
+
+
+def write(request):
+    return shortcuts.render_to_response('artillery/write.html', {})
