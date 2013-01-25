690feafa87c752770ae5ca6eecb86da476bd64fa
Tony Hauber <thauber@gmail.com>
2013-01-24 23:18:45 -0800

    Created api call subbed_stories_for_user

--- python/artillery/api.py
+++ python/artillery/api.py
@@ -12,6 +12,7 @@ def token(data):
 class DictFields:
     UserFields = ('username', 'first_name', 'last_name')
     SubscriptionFields = ('token',)
+    ChapterFields = ('title', 'story', 'order', 'content', )
     
 @repeatable
 def dictify(things, fields):
@@ -46,14 +47,28 @@ def get_users(uids):
     users = get_list(User, uids)
     return dictify_users(users)
 
-# Subscription
+# Chapter
+@repeatable
+def dictify_chapter(chapters):
+    return dictify(chapters, DictFields.ChapterFields)
 
+# Subscription
 def create_subscription(canon_id, user_id):
     sub = models.Subscription.get_or_create(
         user_id=user_id, canon_id=canon_id)
-    return dictify_sub(sub)
+    return sub
 
+def subbed_stories_for_user(uid):
+    subs = Subscription.objects.filter(user_id=uid)
+    sub_ids = [sub.id for sub in subs]
+    stories = Story.objects.filter(cannon__subscription__id__in = sub_ids)
+    return stories
+
+# Story
 @repeatable
-def dictify_subscription(subs):
-    sub_dicts = dictify(users, DictFields.SubscriptionFields)
+def dictify_stories(stories, viewers=None):
+    story_dicts = dictify_stories(stories)
+    for sub, sub_dict, story_dict in zip(subs, sub_dicts, story_dicts):
+        chapter = sub.canon.story.chapter.order_by('-created_on')[0]
+        sub_dict['latest_chapter'] = dictify_chapter(chapter)
     return sub_dicts
